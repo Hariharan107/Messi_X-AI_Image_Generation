39d43dd465d47393ed18de5b1dd8da47970e8ef1
Hariharan <hariharen107@gmail.com>
2023-01-29 16:38:05 +0530

    real backend deployed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -23,7 +23,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://messi-x-dall-e.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -49,7 +49,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImage(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://messi-x-dall-e.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://messi-x-dall-e.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
