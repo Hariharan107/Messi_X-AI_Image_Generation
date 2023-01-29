439fd62b13a97773463beea9627b37b6cd25d0bd
Hariharan <hariharen107@gmail.com>
2023-01-29 16:19:21 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,10 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import messi from './messi.svg'
+export {
+  download,
+  logo,
+  preview,
+  messi,
+};
---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const FormField = ({labelName,type,name,placeholder,value,handleChange,isSurpriseMe,handleSupriseMe,}) => (
+  <div>
+    <div className="flex items-center gap-2 mb-2">
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium text-gray-900 pr-1"
+      >
+        {labelName}
+      </label>
+      {isSurpriseMe && (
+        <button
+          type="button"
+          onClick={handleSupriseMe}
+          className="font-semibold text-xs bg-[#6469FF] py-1 px-2 rounded-[5px] text-white"
+        >
+          Surprise me
+        </button>
+      )}
+    </div>
+    <input
+      type={type}
+      id={name}
+      name={name}
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+      placeholder={placeholder}
+      value={value}
+      onChange={handleChange}
+      required
+    />
+  </div>
+);
+
+export default FormField;
